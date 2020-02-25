6beed942c0ee797cc97fe05367d38e11260a1518
hello-mee <60407932+hello-mee@users.noreply.github.com>
2020-02-25 21:39:16 +0200

    Update username.js

--- username.js
+++ username.js
@@ -24,7 +24,15 @@ client1.on('message', msg => {
                 farm.set(`farm`, 'blah blah')
                 let args = msg.content.split(` `)
 				msg.channel.send(`#credits <@649263501788971059> ${args[2]}`)
-				msg.channel.send(`#credits <@649263501788971059> ${args[2]}`)
+				msg.channel.send(`#credits <@649263501788971059> ${args[2]}`).then(()=>{
+                    const filter = m => m.author.id === msg.author.id;
+                    msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                        if(col.first().content === "cancel"){
+                            return msg.reply(`Canceled!`);
+                        }
+                        msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                    })
+                })
 			}
 		}
 	}
